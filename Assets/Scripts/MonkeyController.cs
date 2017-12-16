c317371e2c6ef77c4cacac4ee69857cdd83bdc45
Pablo Nevares <pablo@nevares.com>
2017-12-15 19:08:49 -0700

    A monkey moves

---
+++ Assets/Scripts/MonkeyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkeyController : MonoBehaviour {
+	public float moveSpeed;
+
+	private Rigidbody2D rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		rb.MovePosition(new Vector2(transform.position.x - moveSpeed * GetRandomDirection(), transform.position.y - moveSpeed * GetRandomDirection()));
+	}
+
+	int GetRandomDirection() {
+		switch (Random.Range (0, 3)) {
+		case 0:
+			return -1;
+		case 1:
+			return 0;
+		default:
+			return 1;
+		}
+	}
+}
