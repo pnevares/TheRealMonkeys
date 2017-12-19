1ef809cc050364a0b99f374da4b7bda9e7ebb7ec
Pablo Nevares <pablo@nevares.com>
2017-12-19 14:53:40 -0700

    Background music

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+    private static MusicController instance = null;
+
+    public static MusicController Instance {
+        get { return instance; }
+    }
+
+	void Awake () {
+        if (instance != null && instance != this) {
+            Destroy (this.gameObject);
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad (this.gameObject);
+	}
+}
