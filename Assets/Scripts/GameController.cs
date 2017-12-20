20e7c29f6ff38dc2ccdd656a3642e4e7228a1320
Pablo Nevares <pablo@nevares.com>
2017-12-20 11:54:14 -0700

    Removed text-based results, new background music

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,17 +7,13 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour {
     [Range(4, 8)]
     public int monkeyCount;
-    public Text resultsText;
     public GameObject monkey;
     public GameObject doneButton;
 
-    private bool noText = true;
     private bool gameOver = false;
     private List<GameObject> monkeys;
 
     private void Start () {
-        resultsText.text = "";
-
         int realMonkeys = Random.Range (1, 4);
 
         monkeys = new List<GameObject> ();
@@ -33,37 +29,14 @@ public class GameController : MonoBehaviour {
     public void Done() {
         doneButton.SetActive (false);
 
-        if (noText) {
-            Vector2 monkeyPosition = new Vector2 (-5.5f, 3.5f);
-            foreach (GameObject monkey in monkeys) {
-                MonkeyController monkeyController = monkey.GetComponent<MonkeyController> ();
+        Vector2 monkeyPosition = new Vector2 (-5.5f, 3.5f);
+        foreach (GameObject monkey in monkeys) {
+            MonkeyController monkeyController = monkey.GetComponent<MonkeyController> ();
 
-                gameOver = true;
-                monkeyController.SetSpeed (3f);
-                monkeyController.SetDestination (monkeyPosition);
-                monkeyPosition.x += 1.5f;
-            }
-        } else {
-            int realMonkeysFound = 0;
-            int realMonkeysMissed = 0;
-            int fakeMonkeysSelected = 0;
-            foreach (GameObject monkey in monkeys) {
-                MonkeyController monkeyController = monkey.GetComponent<MonkeyController> ();
-                if (monkeyController.IsReal ()) {
-                    if (monkeyController.IsSelected ()) {
-                        realMonkeysFound++;
-                    } else {
-                        realMonkeysMissed++;
-                    }
-                } else if (monkeyController.IsSelected ()) {
-                    fakeMonkeysSelected++;
-                }
-                Destroy (monkey);
-            }
-
-            resultsText.text = realMonkeysFound + " real monkeys found\n" +
-            realMonkeysMissed + " real monkeys missed\n" +
-            "Fooled by " + fakeMonkeysSelected + " fake monkeys";
+            gameOver = true;
+            monkeyController.SetSpeed (3f);
+            monkeyController.SetDestination (monkeyPosition);
+            monkeyPosition.x += 1.5f;
         }
     }
 
