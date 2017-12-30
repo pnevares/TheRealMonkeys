9f80be4bd98f88544b958f348aa76447bf23ac82
Pablo Nevares <pablo@nevares.com>
2017-12-29 20:57:29 -0700

    Removed unused assets, added Credits scene, updated icon

---
+++ Assets/Scripts/buttons/CreditsButtonController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsButtonController : MonoBehaviour {
+    [SerializeField]
+    private SpriteRenderer sprite;
+
+    private void OnMouseDown() {
+        SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+    }
+
+    private void OnMouseEnter() {
+        sprite.color = Color.white;
+    }
+
+    private void OnMouseExit() {
+        sprite.color = Color.black;
+    }
+}
